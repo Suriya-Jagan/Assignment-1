3091200677ea59ced8b24fe3cc3f22cfee913035
Suriya J <128624107+Suriya-Jagan@users.noreply.github.com>
2023-08-24 07:40:06 +0530

    Update Problem1.py

--- Assignment-2/Problem1.py
+++ Assignment-2/Problem1.py
@@ -1,1 +1,21 @@
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
 
+tuples = int(input("Enter the number of tuples: "))
+l = []
+for i in range(1, tuples+1):
+    l2 = []
+    tup = input(f"Enter the 1st ele in tuple for list {i}: ")
+    tup1 = input(f"Enter the 2nd ele in tuple for list {i}: ")
+    l2.append(tup)
+    l2.append(tup1)
+    l3 = tuple(l2)
+    l.append(l3)
+    
+for i in range(len(l)):
+    for j in range(i+1, len(l)):
+        if l[i][1] > l[j][1]:
+            l[i], l[j] = l[j], l[i]
+print(l)
+print(len(l))
