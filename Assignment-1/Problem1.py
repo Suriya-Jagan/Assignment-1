14754a348f5457b6796473d5b8d1fe09f0f50988
Suriya J <128624107+Suriya-Jagan@users.noreply.github.com>
2024-05-14 08:24:23 +0530

    Update Problem1.py

--- Assignment-1/Problem1.py
+++ Assignment-1/Problem1.py
@@ -5,12 +5,13 @@
 # Every next number is found by adding up the two numbers before it.
 # Expected Output : 1 1 2 3 5 8 13 21 34
 
+user_input = input(int('Enter a number to find a fibonacci series within that: '))
 x = 0
 y = 1
 fibonacci = []
 strfib = []
 
-while x <=50:
+while x <=user_input:
     fibonacci.append(x)
     x, y = y, x + y
 
